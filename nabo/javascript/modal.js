32c486ad2154d458736c6e6ab4c55a8d0bcbc99c
hjstalder <hansi.stalder@gmail.com>
2017-05-14 17:41:20 +0200

    modal text for parameter and land use

--- nabo/javascript/modal.js
+++ nabo/javascript/modal.js
@@ -2,21 +2,23 @@
 // Organize the modal boxes 
 // ***********************
 function modalbox_parameter () {
-	console.log("modalbox: ", d3.select("#subID").property("value"));
 	document.getElementById('modaltitel').innerHTML = d3.select("#subID").property("value");
 
-	document.getElementById('modaltext').innerHTML = "<a href='" + window[setLanguage()]["glossarUrl"] + "'" + "target='_blank'>" + 
-    window[setLanguage()]["glossarUrlText"] + "</a>";	
-
-    document.getElementById('loreipsum').innerHTML = "";
+	if(d3.select("#subID").property("value") != null && d3.select("#subID").property("value") != "") {
+		document.getElementById('modaltext').innerHTML = window[setLanguage()][paramMapping[d3.select("#subID").property("value")]+ "_descr"];
+	} else {
+		document.getElementById('modaltext').innerHTML = window[setLanguage()]["noHeavyMetal"];
+	}	
 }
 
 function modalbox_landuse () {
-	console.log("modalbox: ", d3.select("#landuseID").property("value"));
-	console.log("modalbox1: ", document.getElementById('modaltitel'));
-	document.getElementById('modaltitel').innerHTML = window[setLanguage()]["landusetext0"];
-	document.getElementById('modaltext').innerHTML = window[setLanguage()]["landusetext1"];
-	document.getElementById('loreipsum').innerHTML = "";
+	document.getElementById('modaltitel').innerHTML = d3.select("#landuseID").property("value");
+
+	if(d3.select("#landuseID").property("value") != null && d3.select("#landuseID").property("value") != "") {
+		document.getElementById('modaltext').innerHTML = landuseDescription(d3.select("#landuseID").property("value"));
+	} else {
+		document.getElementById('modaltext').innerHTML = window[setLanguage()]["noLanduse"];
+	}	
 }
 
 function modalbox_municipality (selectedMunicipality) {
@@ -46,4 +48,48 @@ function modalbox_handling () {
       document.getElementById('handlingTextVis').innerHTML = window[setLanguage()]["hometext6"];
     }
 
+}
+
+function landuseDescription(landuse) {
+	
+	switch (landuse) {
+      	case window[setLanguage()]["cropCultivation"]:
+        	return window[setLanguage()]["cropCultivation_descr"];
+        	break;
+      	case window[setLanguage()]["vegetableGrowing"]:
+        	return window[setLanguage()]["vegetableGrowing_descr"];
+        	break;
+      	case window[setLanguage()]["grasslandIntensiv"]:
+        	return window[setLanguage()]["grasslandIntensiv_descr"];
+        	break;
+      	case window[setLanguage()]["grasslandExtensiv"]:
+        	return window[setLanguage()]["grasslandExtensiv_descr"];
+        	break;
+      	case window[setLanguage()]["grasslandLessIntensive"]:
+        	return window[setLanguage()]["grasslandLessIntensive_descr"];
+        	break;
+      	case window[setLanguage()]["deciduousForest"]:
+        	return window[setLanguage()]["deciduousForest_descr"];
+        	break;
+      	case window[setLanguage()]["mixedForest"]:
+        	return window[setLanguage()]["mixedForest_descr"];
+        	break;
+      	case window[setLanguage()]["coniferousForest"]:
+        	return window[setLanguage()]["coniferousForest_descr"];
+        	break;
+      	case window[setLanguage()]["fruitGrowing"]:
+        	return window[setLanguage()]["fruitGrowing_descr"];
+        	break;
+      	case window[setLanguage()]["viticulture"]:
+        	return window[setLanguage()]["viticulture_descr"];
+        	break;
+      	case window[setLanguage()]["protectedSite"]:
+        	return window[setLanguage()]["protectedSite_descr"];
+        	break;
+      	case window[setLanguage()]["urbanPark"]:
+        	return window[setLanguage()]["urbanPark_descr"];
+        	break;
+      	default:
+        	return window[setLanguage()]["noLanduse"];
+    }
 }
